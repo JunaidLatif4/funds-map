e25bf718aacea5ca4fbba9e9ff8590e1aa0673d1
Zahid Ghafoor <zahidghafoor40@gmail.com>
2021-08-06 19:05:53 +0500

    update-demat page added

--- src/screens/Demet/UploadDemet/UploadDemet.jsx
+++ src/screens/Demet/UploadDemet/UploadDemet.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import upload from "../../../Assets/imgs/upload.svg";
+import { Link } from "react-router-dom";
+import "./UploadDemet.css";
 
 const UploadDemet = () => {
   return (
     <>
-      <div className="psignup__confirm">
-        <div className="psignup__checkbox">
-          <img src={upload} alt="" />
+      <div className="updemet__container" style={{ padding: "0 20px" }}>
+        <div className="updemat__header">
+          <div className="updemat__img">
+            <img
+              style={{ width: "40px", height: "36px", marginRight: "6px" }}
+              src={upload}
+              alt=""
+            />
+          </div>
+          <Link className="updemat__headerpara">Upload CMR / CML</Link>
         </div>
-        <div className="Psignup__confirmline">
-          I confirm that the above bank account belongs to me.
+
+        <div
+          className="updemet__para2"
+          style={{ fontSize: "11px", color: "#8a8a8a" }}
+        >
+          You can get your CMR / CML from your stock broker / depository
+          participant. <Link> Click here to know more</Link>
         </div>
       </div>
     </>
